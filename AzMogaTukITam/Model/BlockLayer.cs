3ea872c85cdb864faf69be5866d50d4da16714c7
Kaloyan Sarafov <kaloyan@sarafov.email>
2022-06-04 14:55:49 +0300

    refactorink

--- AzMogaTukITam/Model/BlockLayer.cs
+++ AzMogaTukITam/Model/BlockLayer.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 
-namespace AzMogaTukITam.Model
+namespace AzMogaTukITam.Model;
+
+public class BlockLayer : LayerBase
 {
-    public class BlockLayer : LayerBase
+    public BlockLayer(Grid grid) : base(grid)
     {
-        public BlockLayer(Grid grid) : base(grid)
-        {
-        }
-
-        public override int ZIndex { get; protected set; } = 100;
-        public override DisplayValue DisplayValue { get; protected set; } = new DisplayValue() {Value = ' ', DisplayBackground = ConsoleColor.DarkGray};
-        public override bool[,] Data { get; protected set; }
-        public override int ConsolePriority { get; protected set; } = 0;
-        public override int RequiredTurns { get; protected set; } = 0;
-        public override Action<Game, ConsoleKeyInfo> ConsoleAction { get; protected set; }
-        public override Action<Game> UpdateAction { get; protected set; }
-
-        // Override props
-
-        public void Block(Coordinates cords) => this.Data[cords.Y, cords.X] = true;
     }
+
+    public override int ZIndex { get; protected set; } = 100;
+    public override DisplayValue DisplayValue { get; protected set; } = new() {Value = ' ', DisplayBackground = ConsoleColor.DarkGray};
+    public override bool[,] Data { get; protected set; }
+    public override int ConsolePriority { get; protected set; } = 0;
+    public override int RequiredTurns { get; protected set; } = 0;
+    public override Action<Game, ConsoleKeyInfo> ConsoleAction { get; protected set; }
+    public override Action<Game> UpdateAction { get; protected set; }
+
+    // Override props
+
+    public void Block(Coordinates cords) => Data[cords.Y, cords.X] = true;
 }
