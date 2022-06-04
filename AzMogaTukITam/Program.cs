59b18fca3f00923c97b788a0ec592a78df49bd6c
Kardam Simeonov <masutayodagm@gmail.com>
2022-06-04 13:52:34 +0300

    Fixed main menu screen

--- AzMogaTukITam/Program.cs
+++ AzMogaTukITam/Program.cs
@@ -1,6 +1,63 @@
 ﻿using AzMogaTukITam.Model;
 
-Console.WriteLine(@"
+PrintLogo();
+Console.WriteLine("Welcome to Queens, please enter a number corresponding to one of the available options below");
+Console.WriteLine("1. Single Player");
+Console.WriteLine("2. Multi Player");
+Console.WriteLine("3. Exit");
+var selection = Console.ReadLine();
+int[] gridSize;
+
+switch (selection)
+{
+    case "1":
+        gridSize = ReadGridSize();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Sry, not implemented!");
+        Console.ForegroundColor = ConsoleColor.White;
+        break;
+    case "2":
+        gridSize = ReadGridSize();
+        Game mainGame = new Game(gridSize[0], gridSize[1], new GameContext());
+        mainGame.Grid.Layers.Add(new BaseLayer(mainGame.Grid));
+        mainGame.Grid.Layers.Add(new BlockLayer(mainGame.Grid));
+        mainGame.Grid.Layers.Add(new PlayerLayer(mainGame.Grid, new DisplayValue() { DisplayBackground = ConsoleColor.DarkBlue, DisplayForeground = ConsoleColor.White }, "Player 1"));
+        mainGame.Grid.Layers.Add(new PlayerLayer(mainGame.Grid, new DisplayValue() { DisplayBackground = ConsoleColor.DarkRed, DisplayForeground = ConsoleColor.White }, "Player 2"));
+        mainGame.Grid.Layers.Add(new SelectedLayer(mainGame.Grid));
+        mainGame.Start();
+        break;
+    case "3":
+        break;
+    default:
+        Console.WriteLine("Invalid selection");
+        break;
+}
+
+
+static int[] ReadGridSize()
+{
+    while (true)
+    {
+        Console.Clear();
+        PrintLogo();
+        try
+        {
+            Console.WriteLine("Please enter the grid dimensions in the format: '[width],[height]'");
+            var sizes = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+            if (sizes.Length != 2 || sizes.Any(x => x > 21 || x < 3)) throw new Exception();
+            return sizes;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Grid must be at least 3x3 and up to 20x20!");
+            Thread.Sleep(3000);
+        }
+    }
+}
+
+static void PrintLogo()
+{
+    Console.WriteLine(@"
          _______                   _____                    _____                    _____                    _____                    _____          
         /::\    \                 /\    \                  /\    \                  /\    \                  /\    \                  /\    \         
        /::::\    \               /::\____\                /::\    \                /::\    \                /::\____\                /::\    \        
@@ -24,51 +81,4 @@ Console.WriteLine(@"
          ~~                       ~~                       \/____/                  \/____/                  \/____/                  \/____/         
                                                                                                                                                       
 ");
-
-Console.WriteLine("Welcome to Queens, please enter a number corresponding to one of the available options below");
-Console.WriteLine("1. Single Player");
-Console.WriteLine("2. Multi Player");
-Console.WriteLine("3. Exit");
-var selection = Console.ReadLine();
-int[] gridSize = ReadGridSize();
-
-switch (selection)
-{
-    case "1":
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Sry, not implemented!");
-        Console.ForegroundColor = ConsoleColor.White;
-        break;
-    case "2":
-        Game mainGame = new Game(gridSize[0], gridSize[1], new GameContext());
-        mainGame.Grid.Layers.Add(new BaseLayer(mainGame.Grid));
-        mainGame.Grid.Layers.Add(new BlockLayer(mainGame.Grid));
-        mainGame.Grid.Layers.Add(new PlayerLayer(mainGame.Grid, new DisplayValue() { DisplayBackground = ConsoleColor.DarkBlue, DisplayForeground = ConsoleColor.White }, "Player 1"));
-        mainGame.Grid.Layers.Add(new PlayerLayer(mainGame.Grid, new DisplayValue() { DisplayBackground = ConsoleColor.DarkRed, DisplayForeground = ConsoleColor.White }, "Player 2"));
-        mainGame.Grid.Layers.Add(new SelectedLayer(mainGame.Grid));
-        mainGame.Start();
-        break;
-    case "3":
-        break;
-    default:
-        Console.WriteLine("Invalid selection");
-        break;
-}
-
-
-static int[] ReadGridSize()
-{
-    while (true)
-    {
-        try
-        {
-            Console.WriteLine("Grid must be at least 3x3 and up to 20x20!");
-            var sizes = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
-            if (sizes.Length != 2 || sizes.Any(x => x > 21 || x < 3)) throw new Exception();
-            return sizes;
-        }
-        catch (Exception ex)
-        {
-        }
-    }
 }
