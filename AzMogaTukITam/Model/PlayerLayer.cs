e71a147bcd6ee376a341bb38530122e7a57a5eed
Kardam Simeonov <masutayodagm@gmail.com>
2022-06-04 10:09:27 +0300

    Added Methods and Vars to PlayerLayer

--- AzMogaTukITam/Model/PlayerLayer.cs
+++ AzMogaTukITam/Model/PlayerLayer.cs
@@ -4,6 +4,10 @@ namespace AzMogaTukITam.Model
 {
     public class PlayerLayer : LayerBase
     {
+        static HashSet<int> attackedRows = new HashSet<int>();
+        static HashSet<int> attackedColumns = new HashSet<int>();
+        static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        static HashSet<int> attackedRightDiagonals = new HashSet<int>();
 
         public PlayerLayer(Grid grid) : base(grid)
         {
@@ -16,6 +20,37 @@ namespace AzMogaTukITam.Model
         {
             this.DisplayValue = dv;
             this.PlayerName = pn;
+        }
+
+        private bool CanPlaceQueen(int row, int col, Grid grid)
+        {
+            var playerLayers = grid.Layers
+                .Where(x => x is PlayerLayer && x.LayerID != this.LayerID) 
+                as IEnumerable<PlayerLayer>;
+
+
+            foreach (var layer in playerLayers)
+            {
+                var positionOccupied =
+                attackedRows.Contains(row) ||
+                attackedColumns.Contains(col) ||
+                attackedLeftDiagonals.Contains(col - row) ||
+                attackedRightDiagonals.Contains(col + row);
+
+                if (positionOccupied)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void MarkPositions(int row, int col)
+        {
+            attackedRows.Add(row);
+            attackedColumns.Add(col);
+            attackedLeftDiagonals.Add(col - row);
+            attackedRightDiagonals.Add(col + row);
+            this.Data[row, col] = true;
         }
 
         public override int ZIndex { get; protected set; } = 0;
